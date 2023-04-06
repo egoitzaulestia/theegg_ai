44812f6e621d197627d2eb73786ed5bf1636a0be
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-04-06 17:46:50 +0200

    Create tiempo.py

---
+++ tarea_94/tiempo.py
@@ -0,0 +1,11 @@
+def tierra(t):
+    z = (1/2) * 9.8 * (int(t) * 2)
+    return f"{z:.2f}"
+
+def marte(t):
+    z = (1 / 2) * 3.7 * (int(t) * 2)
+    return f"{z:.2f}"
+
+def jupiter(t):
+    z = (1 / 2) * 24.8 * (int(t) * 2)
+    return f"{z:.2f}"
