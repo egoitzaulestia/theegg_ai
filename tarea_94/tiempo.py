e88db987c7c4a8c88c6ee2f49ebda41958135d05
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-04-06 17:49:40 +0200

    Update tiempo.py

--- tarea_94/tiempo.py
+++ tarea_94/tiempo.py
@@ -1,3 +1,6 @@
+# Módulos tiempo.py
+
+# definiciones de tierra, marte y jupiter para averiguarl a cuantp altura estaría un objeto si introducimos los segunfos de caída de objeto.
 def tierra(t):
     z = (1/2) * 9.8 * (int(t) * 2)
     return f"{z:.2f}"
