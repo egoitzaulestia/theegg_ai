c11b016dcb68fe3dce6ebef2dba04a39e8b805a1
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-04-28 01:05:59 +0200

    Create tarea_93_teoria.py

---
+++ tarea_93/tarea_93_teoria.py
@@ -0,0 +1,18 @@
+# BUENAS PRÁCTICAS A LA HORA DE PROGRAMAR
+
+"""
+
+1. Define el alcance del proyecto: es importante redactar un documento que defina el alcance donde todos los participantes estén de acuerdo.
+2. Elección de una interfaz de desarrollo que se adapte a tus necesidades: sublime text, visual studio code, atom, etcétera.
+3. Documentación: durante los desarrollos intenta documentar todo.
+4. Estandariza las reglas del desarrollo: debes acordar con el equipo de desarrollo la forma de trabajar. Tanto las reglas de estilo del código como la forma de trabajar deben seguir unas normas que deben ser respetadas por todo el equipo.
+5. Divide y vencerás I: es preciso dividir los desarrollos en fases y/o entregables. Esta división debe permitir que el cliente pueda ver resultados durante el desarrollo. 
+6. Divide y vencerás II: divide los desarrollos complejos en varios más sencillos.
+7. No inventes: si ya existen módulos que funcionan, ¿para que vas a perder tiempo en reinventar la rueda?
+8. Dont repeat yourself: no repitas código, modulariza tus desarrollos.
+9. Comenta tu código: redacta sentencias simples e intenta elaborar una solución sencilla y corta. 
+10. Seguridad: durante el desarrollo de nuestro software debemos prever posibles vulnerabilidades y darles una solución.
+11. Optimiza el código: hay muchos caminos para llegar a un mismo fin. Intenta coger el camino más corto.
+12. Testeo de código: valida, testea el código antes de llevarlo a producción. 
+
+"""
